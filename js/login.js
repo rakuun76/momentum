2a8389a7923103613e271fbd2d502e6d49341362
rakuun76 <rakuun76@gmail.com>
2024-04-29 21:53:02 +0900

    login css 작업

--- js/login.js
+++ js/login.js
@@ -1,26 +1,28 @@
-const loginForm = document.querySelector(".login-form");
-const loginFormInput = document.querySelector(".login-form__input");
-const loginGreeting = document.querySelector(".login-greeting");
+const loginDiv = document.querySelector(".login");
+const loginForm = document.querySelector(".login__form");
+const loginInput = document.querySelector(".login__input");
+const mainContentDiv = document.querySelector(".main-content");
+const usernameDiv = document.querySelector(".username");
 
 const USERNAME_KEY = "username";
 const HIDDEN_CLASSNAME = "hidden";
 
-const paintGreetings = (username) => {
-  loginGreeting.innerText = `Hello ${username}`;
-  loginGreeting.classList.remove(HIDDEN_CLASSNAME);
+const paintUsername = (username) => {
+  usernameDiv.innerText = `${username}`;
+  mainContentDiv.classList.remove(HIDDEN_CLASSNAME);
 };
 
 const handleLoginFormSubmit = (event) => {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERNAME_KEY, loginFormInput.value);
-  paintGreetings(loginFormInput.value);
+  loginDiv.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintUsername(loginInput.value);
 };
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername) {
-  paintGreetings(savedUsername);
+  paintUsername(savedUsername);
 } else {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginDiv.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", handleLoginFormSubmit);
 }
